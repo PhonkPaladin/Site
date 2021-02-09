cd4ac5b2f94125368dda0d0a4a0a15b794fbfe25
stasDom <valariot@gmail.com>
2021-02-10 00:03:14 +0300

    alert

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.querySelector('#home').onclick = () => {
+    alert('ТЫ ШО ПОЕХАВШИЙ СЮДА НАЖИМАТЬ?');
+}
+
+document.querySelector('#anime').onclick = () => {
+    alert('ДА НЕ ГОТОВО ЕЩЕ!');
+}
+
+document.querySelector('#contact').onclick = () => {
+    alert('МОЖЕТ НЕ НАДО?');
+}
+
+document.querySelector('#about').onclick = () => {
+    alert('ОЙ ВСЕ');
+}
+
+document.querySelector('#help').onclick = () => {
+    let i = 0;
+    while (i < 1) {
+        alert('НУ ТЫ САМ НАПРОСИЛСЯ');
+    }
+}
